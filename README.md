6db42615fd86eabf24e15612eef5d6973b1202d5
FauzyZachiOctari <fauzizakioktari12@gmail.com>
2023-08-04 01:50:41 +0700

    Add Files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fauzyzachi.github.io
---
+++ javascript/script.js
@@ -0,0 +1,82 @@
+
+
+ // hamburger = document.querySelector(".hamburger");
+ //        hamburger.onclick = function(){
+ //            navBar = document.querySelector(".nav-bar");
+ //            navBar.classList.toggle("active");
+ //        };
+
+var TxtType = function(el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 2000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
+
+	if (this.isDeleting) {
+		this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+		this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
+
+	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+	var that = this;
+	var delta = 200 - Math.random() * 100;
+
+	if (this.isDeleting) { delta /= 2; }
+
+	if (!this.isDeleting && this.txt === fullTxt) {
+		delta = this.period;
+		this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+		this.isDeleting = false;
+		this.loopNum++;
+		delta = 500;
+	}
+
+	setTimeout(function() {
+		that.tick();
+	}, delta);
+};
+
+window.onload = function() {
+	var elements = document.getElementsByClassName('typewrite');
+	for (var i=0; i<elements.length; i++) {
+		var toRotate = elements[i].getAttribute('data-type');
+		var period = elements[i].getAttribute('data-period');
+		if (toRotate) {
+			new TxtType(elements[i], JSON.parse(toRotate), period);
+		}
+	}
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ffffff}";
+    document.body.appendChild(css);
+};
+
+function openSkill(evt, skillName) {
+	var i, container, tablinks;
+	container = document.getElementsByClassName("container");
+	for (i = 0; i < container.length; i++) {
+		container[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(skillName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+
+// Script email
+
+
